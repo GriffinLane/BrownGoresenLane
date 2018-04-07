94fda06de63e616e30d6f644ca93afec9be343e5
GriffinLane <31805782+GriffinLane@users.noreply.github.com>
2018-04-07 17:30:09 -0400

    griffin

--- GetMovies.py
+++ GetMovies.py
@@ -20,24 +20,28 @@ print('tmdbsimple version ' + tmdb.__version__)
 def GetRawMovies():
     if(not os.path.exists(os.path.dirname(os.path.realpath("Movies.csv")+"/Movies.csv"))):
         i,g = 1,0 #1 throws error on tmdb
-        totalMoviesRequested = 5
+        totalMoviesRequested = 10000
         raw_movies = list()
         items_requested = ['revenue','vote_average','vote_count'
                            ,'title','original_language','release_date'
-                           ,'production_companies','production_countries'
-                           ,'genres']
+                           ,'production_companies','production_countries','genres']
         keep_mining = True
         bar = prog.ProgressBar(max_value=totalMoviesRequested)
         while(keep_mining):
             try:
-                raw_movie_data = tmdb.Movies(i).info() 
+                raw_movie_data = tmdb.Movies(i).info()
+                if "False" not in str(raw_movie_data.get("adult")):
+                    raise ValueError("we don't need to include adult films")
                 sifted_data = list()
-                for key in items_requested:
+                for key in items_requested:                        
                     if key in ['production_companies','production_countries','genres']:
                         count = len(raw_movie_data.get(key))
                         attribValue = str("")
                         for j in range(count):
-                            attribValue += raw_movie_data.get(key)[j]["name"] + ( "" if j == (count-1) else ",")
+                            attribValue += raw_movie_data.get(key)[j]["name"] + ( "" if j == (count-1) else ",")                        
+                        if key is "production_countries":
+                            if "United States of America" not in attribValue:
+                                raise ValueError("not made in America")
                         sifted_data.append(attribValue)
                     else:
                         sifted_data.append(raw_movie_data.get(key))
@@ -54,10 +58,7 @@ def GetRawMovies():
     return None
 
 raw_movies = GetRawMovies()
-#cross with IMDb
 
-
-#db.search_movie("braveheart")
 if(raw_movies is not None):
     pd.DataFrame(data=raw_movies, columns = ['revenue',
                                          'vote_average',
@@ -68,3 +69,13 @@ if(raw_movies is not None):
                                          'production_companies',
                                          'production_countries',
                                          'genres']).to_csv('Movies.csv',index=False,header=True)
+
+####cross with IMDb
+#Get list of titles to search IMDb for
+raw_movies = pd.read_csv('Movies.csv')
+for i in range(len(raw_movies)):
+    print(raw_movies["title"][i])
+
+
+#db.search_movie("braveheart")
+#raise ValueError('A very specific bad thing happened.')
